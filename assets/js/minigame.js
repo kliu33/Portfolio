1f432951a2fbfaf0735b4efdf94a52687ccdbf06
kliu33 <kliu33@buffalo.edu>
2023-04-14 10:19:04 -0400

    added little minigame, style needs work and color

---
+++ assets/js/minigame.js
@@ -0,0 +1,20 @@
+function collectGame() {
+    var game = document.getElementById("game")
+    if (game.getAttribute('src') === './images/game-off.png') {
+        game.src = './images/game-on.png'
+    }
+}
+
+function collectApple() {
+    var apple = document.getElementById("apple")
+    if (apple.getAttribute('src') === './images/apple-off.png') {
+        apple.src = './images/apple-on.png'
+    }
+}
+
+function collectRuby() {
+    var ruby = document.getElementById("ruby")
+    if (ruby.getAttribute('src') === './images/ruby-off.png') {
+        ruby.src = './images/ruby-on.png'
+    }
+}
