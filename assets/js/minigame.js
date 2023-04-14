f4fa0bbfa2f03d1e68cc23300141af9de8bd95c0
kliu33 <kliu33@buffalo.edu>
2023-04-14 18:08:52 -0400

    added little sparkle ding, fix some styling

--- assets/js/minigame.js
+++ assets/js/minigame.js
@@ -1,7 +1,10 @@
+audio = new Audio('./assets/collect.mp3')
+
 function collectGame() {
     var game = document.getElementById("game")
     if (game.getAttribute('src') === './images/game-off.png') {
         game.src = './images/game-on.png'
+        audio.play();
     }
 }
 
@@ -9,6 +12,7 @@ function collectApple() {
     var apple = document.getElementById("apple")
     if (apple.getAttribute('src') === './images/apple-off.png') {
         apple.src = './images/apple-on.png'
+        audio.play();
     }
 }
 
@@ -16,6 +20,7 @@ function collectRuby() {
     var ruby = document.getElementById("ruby")
     if (ruby.getAttribute('src') === './images/ruby-off.png') {
         ruby.src = './images/ruby-on.png'
+        audio.play();
     }
 }
 
@@ -23,5 +28,6 @@ function collectHd() {
     var hd = document.getElementById("hd")
     if (hd.getAttribute('src') === './images/hd-off.png') {
         hd.src = './images/hd-on.png'
+        audio.play();
     }
 }
